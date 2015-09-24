befa53907d2852af65343c8e9490a5065f620732
748s <nick.wakeman@gmail.com>
2015-09-24 14:31:22 -0400

    Update sheet.py to correctly pull in access.json file

--- main/sheet.py
+++ main/sheet.py
@@ -1,9 +1,11 @@
+import os
 import json
 import gspread
 from oauth2client.client import SignedJwtAssertionCredentials
 
 def get_google_sheet():
-    json_key = json.load(open('./main/access.json'))
+    dir = os.path.dirname(os.path.realpath(__file__))
+    json_key = json.load(open(dir +'/access.json'))
     scope = ['https://spreadsheets.google.com/feeds']
     credentials = SignedJwtAssertionCredentials(json_key["client_email"], json_key['private_key'], scope)
     authorization = gspread.authorize(credentials)
