69e2f1a6b7f32c9cd35a4f8b99b9e6d8f0d71779
748s <nick.wakeman@gmail.com>
2015-09-24 15:51:11 -0400

    Add Callout and Billboard data to JSON file

--- main/sheet.py
+++ main/sheet.py
@@ -10,6 +10,12 @@ def get_google_sheet():
     credentials = SignedJwtAssertionCredentials(json_key["client_email"], json_key['private_key'], scope)
     authorization = gspread.authorize(credentials)
     spreadsheet = authorization.open("VPR Homepage App")
-    worksheet = spreadsheet.get_worksheet(0)
+    vpr_news = spreadsheet.get_worksheet(0)
+    callout = spreadsheet.get_worksheet(1)
+    billboard = spreadsheet.get_worksheet(2)
+    sheets = {}
+    sheets['vpr_news'] = vpr_news.get_all_records()
+    sheets['callout'] = callout.get_all_records()
+    sheets['billboard'] = billboard.get_all_records()
 
-    return worksheet.get_all_records()
+    return sheets
--- main/views.py
+++ main/views.py
@@ -10,13 +10,32 @@ def index():
     os.environ['TZ'] = 'US/Eastern'
     time.tzset()
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-    sheet = get_google_sheet()
+    sheets = get_google_sheet()
     stories = []
-    if len(sheet)>0:
-        for index, story in enumerate(sheet):
+    if len(sheets['vpr_news'])>0:
+        for index, story in enumerate(sheets['vpr_news']):
     	    this_story = {}
     	    this_story['link'] = story['Link']
     	    this_story['story_id'] = story['Story ID']
     	    stories.append(this_story)        
 
-    return render_template('vpr-homepage-app.json',timestamp=timestamp,stories=stories)
+    callout = {}
+    callout['uri'] = sheets['callout'][0]['URI']
+    callout['text'] = sheets['callout'][0]['Text']
+
+    billboard = {}
+    billboard['story_id'] = sheets['billboard'][0]['Story ID']
+    billboard['title'] = sheets['billboard'][0]['Title']
+    billboard['text'] = sheets['billboard'][0]['Text']
+    billboard['link'] = sheets['billboard'][0]['Link']
+    billboard['facebook'] = sheets['billboard'][0]['Facebook']
+    billboard['twitter'] = sheets['billboard'][0]['Twitter']
+    billboard['email'] = sheets['billboard'][0]['Email']
+    billboard['phone'] = sheets['billboard'][0]['Phone']
+	
+    return render_template('vpr-homepage-app.json',
+        timestamp=timestamp,
+        stories=stories,
+        callout=callout,
+        billboard=billboard
+        )
